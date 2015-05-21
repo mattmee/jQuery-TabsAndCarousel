44906a3a0bc04e919f4bd1cba2777f155a9da5a3
Dustin Kraft <kraftdr@gmail.com>
2015-05-21 11:02:42 -0600

    initial

---
+++ jQuery-TabsAndCarousel/jQuery-TabsAndCarousel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jQuery_TabsAndCarousel.Startup))]
+namespace jQuery_TabsAndCarousel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
